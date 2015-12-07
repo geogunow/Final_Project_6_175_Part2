b52a589f3ac835b9fa1a950efeefd54882b05661
Sara Sinback <sinback@mit.edu>
2015-12-07 12:57:00 -0500

    started debugging three-cycle, six-stage procs

--- scemi/sim/run_asm.sh
+++ scemi/sim/run_asm.sh
@@ -8,25 +8,29 @@ fi
 simdut=${1}_dut
 
 asm_tests=(
-	simple
-	add addi
-	and andi
-	auipc
-	beq bge bgeu blt bltu bne
-	j jal jalr
-	lw
-	lui
-	or ori
-	sw
-	sll slli
-	slt slti
-	sra srai
-	srl srli
-	sub
-	xor xori
-	bpred_bht bpred_j bpred_j_noloop bpred_ras
-	cache cache_conflict stq
-	)
+  lw
+  )
+
+#asm_tests=(
+#	simple
+#	add addi
+#	and andi
+#	auipc
+#	beq bge bgeu blt bltu bne
+#	j jal jalr
+#	lw
+#	lui
+#	or ori
+#	sw
+#	sll slli
+#	slt slti
+#	sra srai
+#	srl srli
+#	sub
+#	xor xori
+#	bpred_bht bpred_j bpred_j_noloop bpred_ras
+#	cache cache_conflict stq
+#	)
 
 vmh_dir=../../programs/build/assembly/vmh
 log_dir=logs
